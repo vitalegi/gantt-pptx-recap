2690c7f1dbbcae0abc227769723bc03e33300524
Giorgio Vitale <giorgio.vitale.90@gmail.com>
2019-02-17 23:01:52 +0100

    spring initializr init repo

---
+++ src/main/java/it/vitalegi/ganttpptxrecap/GanttPptxRecapApplication.java
@@ -0,0 +1,13 @@
+package it.vitalegi.ganttpptxrecap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GanttPptxRecapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GanttPptxRecapApplication.class, args);
+	}
+
+}
